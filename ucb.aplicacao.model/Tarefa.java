0ed7b2f86f863f12b32a51ac1867157013ad894f
Felipe Campos Hipólito <felipecamhipolito@gmail.com>
2025-09-03 11:00:41 -0300

    Atualizando tarefa
    Criação do tarefa.java

---
+++ ucb.aplicacao.model/Tarefa.java
@@ -0,0 +1,68 @@
+import java.time.LocalDateTime;
+
+public class Tarefa {
+    
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private boolean completa;
+    private LocalDateTime dataAgora;
+
+    public Tarefa() {
+        this.dataAgora = LocalDateTime.now();
+        this.completa = false;
+    }
+
+    public Tarefa(String titulo){
+        this();
+        this.titulo = titulo;
+    }
+
+    public Tarefa(Long id, String titulo, String descricao, boolean completa){
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.completa = completa;
+        this.dataAgora = LocalDateTime.now();
+    }
+
+    public LocalDateTime getDataAgora(){
+        return dataAgora;
+    }
+
+    public void setDataAgora(LocalDateTime dataAgora) {
+        this.dataAgora = dataAgora;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public boolean isCompleta() {
+        return completa;
+    }
+
+    public void setCompleta(boolean completa) {
+        this.completa = completa;
+    }
+}
