e0fa8602fbf762014eae3b5271c8e11d2888b887
Anna Júlia Correa <seu-email-do-github@gmail.com>
2025-09-10 14:14:26 -0300

    Finalizaçao do codigo

--- ucb.aplicacao.service/TarefaServico.java
+++ ucb.aplicacao.service/TarefaServico.java
@@ -43,4 +43,39 @@ public class TarefaServico {
 
             int opcao = entrada.nextInt();
             entrada.nextLine(); // Consumir quebra de linha
+
+            switch (opcao) {
+                case 1:
+                    System.out.print("Digite a nova tarefa: ");
+                    String novaTarefa = entrada.nextLine();
+                    gerenciador.criarTarefa(novaTarefa);
+                    break;
+
+                case 2:
+                    gerenciador.listarTarefas();
+                    break;
+
+                    case 3:
+                    System.out.print("Digite o índice da tarefa a atualizar: ");
+                    int indiceAtualizar = entrada.nextInt();
+                    entrada.nextLine();
+                    System.out.print("Digite a nova descrição da tarefa: ");
+                    String tarefaAtualizada = entrada.nextLine();
+                    gerenciador.atualizarTarefa(indiceAtualizar, tarefaAtualizada);
+                    break;
+
+                case 4:
+                    System.out.print("Digite o índice da tarefa a remover: ");
+                    int indiceRemover = entrada.nextInt();
+                    entrada.nextLine();
+                    gerenciador.removerTarefa(indiceRemover);
+                    break;
+
+                case 5:
+                    entrada.close();
+                    return;
+            }
+        }
+    }
 }
+
