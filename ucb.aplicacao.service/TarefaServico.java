3db41698417575128239bcac35503363702c0752
Anna Júlia Correa <seu-email-do-github@gmail.com>
2025-09-10 13:37:57 -0300

    Outra parte do codigo feita

--- ucb.aplicacao.service/TarefaServico.java
+++ ucb.aplicacao.service/TarefaServico.java
@@ -1,8 +1,35 @@
 import ucb.aplicacao.model.Tarefa;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 
 public class TarefaServico {
+    private List<String> tarefas;
     
+    public TarefaServico(List<String> tarefasIniciais) {
+        this.tarefas = tarefasIniciais;
+    }
+
+    public void criarTarefa(String tarefa) {
+        tarefas.add(tarefa);
+    }
+
+    public void listarTarefas() {
+        for (int i = 0; i < tarefas.size(); i++) {
+            System.out.println(i + " - " + tarefas.get(i));
+        }
+    }
+
+    public void atualizarTarefa(int indice, String novaTarefa) {
+        tarefas.set(indice, novaTarefa);
+    }
+
+    public void removerTarefa(int indice) {
+        tarefas.remove(indice);
+    }
+
+    public static void main(String[] args) {
+        TarefaServiso gerenciador = new TarefaServiso(new ArrayList<>());
+        Scanner entrada = new Scanner(System.in);
 }
